3f2a647548e34182a1b48cba39eed69c0e4167c5
gguilhermelopes <guilhermelopes275@gmail.com>
2023-09-21 21:02:34 -0300

    feat: basic components and useFetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./Style.css";
+import Header from "./components/Header";
+import Sidenav from "./components/Sidenav";
+import Summary from "./pages/Summary";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Sidenav />
+      <main>
+        <Header />
+        <Summary />
+      </main>
+    </div>
+  );
 }
 
 export default App;
