fc9b2db67a5cff52948e79fcfc19e57f1284ae49
gguilhermelopes <guilhermelopes275@gmail.com>
2023-09-25 15:59:58 -0300

    feat: basic summary

--- src/pages/Summary.tsx
+++ src/pages/Summary.tsx
@@ -1,5 +1,45 @@
+import { useData } from "../context/DataContext";
+
+import styles from "./Summary.module.css";
+
 const Summary = () => {
-  return <div>Summary</div>;
+  const { data } = useData();
+
+  if (!data) return null;
+  return (
+    <section>
+      <div className={`${styles.summary} flex mb`}>
+        <div className="box">
+          <h2>Vendas</h2>
+          <span>
+            {data
+              .filter((venda) => venda.status !== "falha")
+              .reduce((a, b) => a + b.preco, 0)
+              .toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Recebido</h2>
+          <span>
+            {data
+              .filter((venda) => venda.status === "pago")
+              .reduce((a, b) => a + b.preco, 0)
+              .toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Processando</h2>
+          <span>
+            {data
+              .filter((venda) => venda.status === "processando")
+              .reduce((a, b) => a + b.preco, 0)
+              .toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+      </div>
+      <div className="box">Gráficos</div>
+    </section>
+  );
 };
 
 export default Summary;
